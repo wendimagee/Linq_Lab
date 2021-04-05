d993446fd7cf4f1e0cd85e84cc153acf91b8b015
Wendi Magee <wendimagee@gmail.com>
2021-04-05 10:30:41 -0400

    Refactored, implemented lambda expressions

--- Program.cs
+++ Program.cs
@@ -8,41 +8,6 @@ namespace Linq_Lab
     {
         static void Main(string[] args)
         {
-            //int[] nums = { 32, 45, 9, 17, 20, 78, 91 };
-
-            ////method syntax
-            //List<int> odds = nums.Where(x => x % 2 == 1).ToList();
-
-            //foreach(int num in odds)
-            //{
-            //    Console.WriteLine(num);
-            //}
-            //Console.WriteLine();
-            ////query syntax
-            //var odds2 = from num in nums
-            //            where num % 2 == 1
-            //            select num;
-            //foreach (int num in odds2.ToList())
-            //{
-            //    Console.WriteLine(num);
-            //}
-
-            ////find the greatest number
-            //int highestNum = nums.Max();
-            //Console.WriteLine("Highest number");
-            //Console.WriteLine(highestNum);
-            //Console.WriteLine();
-            //Console.WriteLine("Highest numberunder 20");
-            //int under20 = nums.Where(x => x < 20).Max();
-            //Console.WriteLine(under20);
-
-            ////count all the even numbers
-            //Console.WriteLine();
-            //Console.WriteLine("count the even numbers");
-            //int totalEvens = nums.Where(x => x % 2 == 0).Count();
-            //Console.WriteLine(totalEvens);
-            //Console.WriteLine("-----------------------------------");
-            Console.WriteLine("For Nums");
             int[] nums = { 10, 2330, 112233, 10, 949, 3764, 2942 };
             Console.WriteLine("1. Find Minimum Value");
             int num = nums.Min();
@@ -85,40 +50,30 @@ namespace Linq_Lab
             Console.WriteLine("2. Find the oldest student");
             Student s = students.OrderByDescending(x => x.Age).First();
             Console.WriteLine(s.Name);
+
             Console.WriteLine("3. Find the youngest student");
             Student b = students.OrderBy(x => x.Age).First();
             Console.WriteLine(b.Name);
+
             Console.WriteLine("4. Find the oldest student under 25");
             List<Student> notOldies = students.Where(x => x.Age < 25).ToList();
             Student notOld = notOldies.OrderByDescending(x => x.Age).First();
             Console.WriteLine(notOld.Name);
+
             Console.WriteLine("5. Find the students over 21 and with even ages");
             List<Student> fiveList = students.Where(x => x.Age > 21 && x.Age % 2 == 0).ToList();
             foreach(Student f in fiveList)
             {
                 Console.WriteLine(f.Name);
             }
+
             Console.WriteLine("6. Find all teenagers");
             List<Student> teens = students.Where(x => x.Age > 12 && x.Age < 20).ToList();
             foreach(Student t in teens)
             {
                 Console.WriteLine(t.Name);
             }
-            Console.WriteLine("7. Find the students whose names start with a vowel");
-            List<Student> vowelNames = new List<Student>();
-            foreach (Student stud in students)
-            {
-                if (stud.Name.StartsWith('A')|| stud.Name.StartsWith('E')|| stud.Name.StartsWith('I')|| stud.Name.StartsWith('O')|| stud.Name.StartsWith('U'))
-                {
-                    vowelNames.Add(stud);
-                }
-            }
-            foreach(Student stus in vowelNames)
-            {
-                Console.WriteLine(stus.Name);
-            }
 
-            //Nick's version of that last question. Pretty cool!
             char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
             List<Student> vowelStart = students.Where(x => x.Name.ToLower().IndexOfAny(vowels) == 0).ToList();
             Console.WriteLine("Students with names starting with a vowel:");
@@ -127,13 +82,12 @@ namespace Linq_Lab
                 Console.WriteLine(stude.Name);
             }
 
-
-            //Console.WriteLine("get a list of students that can drive");
-            //List<Student> drivers = students.Where(x => x.CanDrive == true).ToList();
-            //foreach(Student a in drivers)
-            //{
-            //    Console.WriteLine(a.Name);
-            //}
+            Console.WriteLine("Get a list of students that can drive");
+            List<Student> drivers = students.Where(x => x.CanDrive == true).ToList();
+            foreach (Student a in drivers)
+            {
+                Console.WriteLine(a.Name);
+            }
         }
     }
 }
